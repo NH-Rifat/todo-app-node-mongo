44e524c5a310c1eb29ac08d7b53aa15040f9d3db
NH-Rifat <rifatr729@gmail.com>
2022-05-19 03:59:17 +0600

    data retrieve successfully done

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjcvj.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db('test').collection('devices');
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    // console.log('database connected');
+    const todoCollection = client.db('todo-app').collection('todos');
+
+    app.get('/todos', async (req, res) => {
+      const query = {};
+      const cursor = todoCollection.find(query);
+      const todos = await cursor.toArray();
+      res.send(todos);
+    });
+
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('todo app running')
-})
+  res.send('todo app running');
+});
 
 app.listen(port, () => {
-  console.log(`Todo app listening ${port}`)
-})
+  console.log(`Todo app listening ${port}`);
+});
